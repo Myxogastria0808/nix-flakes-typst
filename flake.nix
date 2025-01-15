7148f347f47bdae15aec1ed7aa09597ebdd61732
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-01-15 11:02:29 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "nodejs flake sample";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            typst
+            zellij
+            tdf
+          ];
+          shellHook = ''
+            zellij --layout ./config/zellij.yaml
+          '';
+        };
+      }
+    );
+}
